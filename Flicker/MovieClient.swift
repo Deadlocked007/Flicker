d616cefa8d3f09f231feeff247e60db6340a90fc
Deadlocked007 <siraj@Sirajs-MacBook-Pro.local>
2017-11-18 00:23:03 -0600

    Created client request using Swift 4 JSON decoding

--- Flicker/MovieClient.swift
+++ Flicker/MovieClient.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class MovieClient {
-    let sharedInstance = MovieClient()
     
-    func getMovies(endpoint: MovieListEndpoints, success: ([Movie]) -> (), failure: (Error) -> ()) {
-        endpoint.rawValue
+    let sharedInstance = MovieClient()
+    let baseURL = "https://api.themoviedb.org/3/movie/"
+    let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+    
+    func getMovies(endpoint: MovieListEndpoints, page: Int, success: @escaping ([Movie]) -> (), failure: @escaping (Error) -> ()) {
+        let urlString = baseURL + endpoint.rawValue
+        var urlComp = URLComponents(string: urlString)
+        var qItems = [URLQueryItem(name: "api_key", value: apiKey)]
+        qItems.append(URLQueryItem(name: "page", value: String(page)))
+        urlComp?.queryItems = qItems
+        let url = urlComp!.url!
+        let urlSession = URLSession(configuration: .default)
+        let task = urlSession.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                failure(error)
+            } else if let data = data {
+                let decoder = JSONDecoder()
+                guard let movies = try? decoder.decode([Movie].self, from: data) else {
+                    return
+                }
+                success(movies)
+            }
+        }
+        task.resume()
     }
 }
 
